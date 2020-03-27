7ccdf043dec08a14bbb0ce71b862c7b94ab3c2b6
Darius-Ndubi <ndubidarius@gmail.com>
2020-03-27 16:28:48 +0300

    Add config for staging cluster Add config to create staging image Add config to deploy to staging custer

--- ci-scripts/build_docker_image.sh
+++ ci-scripts/build_docker_image.sh
@@ -41,6 +41,41 @@ build_and_push_image() {
 
     fi
 
+    #@--- Build staging image ---@#
+
+    if [[ $TRAVIS_BRANCH == "ACT-692" ]] || \
+        [[ $GITHUB_REF == "refs/heads/ACT-692" ]]; then
+        ECHO "++++++ Build Staging Image +++++++++++"
+        
+        #@--- Set all the required variables ---@#
+        touch .env.deploy
+
+        echo export ACTIVITY_CE_DB_NAME=${ACTIVITY_CE_DB_NAME_STAGING} >> .env.deploy
+        echo export ACTIVITY_CE_DB_USER=${ACTIVITY_CE_DB_USER_DEV} >> .env.deploy
+        echo export ACTIVITY_CE_DB_PASSWORD=${ACTIVITY_CE_DB_PASSWORD_DEV} >> .env.deploy
+        echo export ACTIVITY_CE_DB_HOST=${ACTIVITY_CE_DB_HOST_DEV} >> .env.deploy
+        # Switch to staging var
+        echo export SECRET_KEY=${SECRET_KEY_DEV_ENV} >> .env.deploy
+        echo export DEBUG=${DEBUG} >> .env.deploy
+        echo export DJANGO_ALLOWED_HOSTS=${DJANGO_ALLOWED_HOSTS} >> .env.deploy
+        echo export DB_ENGINE=${DB_ENGINE} >> .env.deploy
+        echo export ACTIVITY_CE_DB_PORT=${ACTIVITY_CE_DB_PORT_DEV} >> .env.deploy
+        echo export EMAIL_PORT=${EMAIL_PORT} >> .env.deploy
+        echo export EMAIL_HOST_USER=${EMAIL_HOST_USER_DEV} >> .env.deploy
+        echo export EMAIL_HOST_PASSWORD=${EMAIL_HOST_PASSWORD_DEV} >> .env.deploy
+        echo export EMAIL_HOST=${EMAIL_HOST} >> .env.deploy
+        echo export GOOGLE_MAP_API_KEY=${GOOGLE_MAP_API_KEY_DEV} >> .env.deploy
+
+        export APPLICATION_ENV=${APPLICATION_ENV_STAGING}
+
+        docker build -t $REGISTRY_OWNER/activity:$APPLICATION_NAME-$APPLICATION_ENV-$TRAVIS_COMMIT -f docker-deploy/Dockerfile .  
+        echo "-------- Building Image Done! ----------"
+
+        echo "++++++++++++ Push Image built -------"
+        docker push $REGISTRY_OWNER/activity:$APPLICATION_NAME-$APPLICATION_ENV-$TRAVIS_COMMIT
+
+    fi
+
     #@--- Logout from docker ---@#
     echo "--------- Logout dockerhub --------"
     docker logout                                                                                                                                          ─╯
